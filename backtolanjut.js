ffe67d56988881530d658cd7cb413405108af1a5
vaniaraissa <54266849+vaniaraissa@users.noreply.github.com>
2019-10-07 20:47:31 +0700

    Create backtolanjut.js

---
+++ backtolanjut.js
@@ -0,0 +1,12 @@
+(function(window, location) {
+    history.replaceState(null, document.title, location.pathname + "#!/history");
+    history.pushState(null, document.title, location.pathname);
+    window.addEventListener("popstate", function() {
+        if (location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function() {
+                location.replace("/p/lanjut.html")
+            }, 0)
+        }
+    }, false)
+}(window, location));
